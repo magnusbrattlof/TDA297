1572c6601eda6c7691d629243b587598a927bcb4
fishur <a.s.e.erlandsson@gmail.com>
2019-02-22 11:50:23 +0100

    lots of code, middle of a feature, going on a lunch-break

--- lab1/ExampleCaster.java
+++ lab1/ExampleCaster.java
@@ -4,38 +4,70 @@ import java.util.ArrayList;
 
 /**
  * Simple example of how to use the Multicaster interface.
- *
+ * 
  * @author Andreas Larsson &lt;larandr@chalmers.se&gt;
+ * @author Erland;
+ * @author Mange;
  */
 public class ExampleCaster extends Multicaster {
 
-    private int[] vectorArray;
+    // locical clock of each process and a buffer used to guarantee FIFO per each process
+    // TODO: might have to redo some of this as the format of all messages are differnet now, probably need to save more data so to also achive total order
+    private int[] localSeqArray; 
     private ArrayList<ExampleMessage> buffer;
+
+    // this works as a int tuple, where the first value is logical clock-value/sequence number and the second is id of proposer
+    private int globalSeq; // aka "A"
+    private int globalID;
+    private int proposeSeq; // aka "P"
+    private int proposeID; 
 
     public void init() {
         mcui.debug("The network has " + hosts + " hosts!");
-        vectorArray = new int[hosts];
+        localSeqArray = new int[hosts];
         buffer = new ArrayList<ExampleMessage>();
-    }
+
+        globalSeq = 0;
+        globalID = 0;
         
+        proposeSeq = 0;
+        proposeID = 0;
+
+    }
+
     /**
      * The GUI calls this module to multicast a message
      */
     public void cast(String messagetext) {
+        ExampleMessage eMessage;
+        // phase 1 -- requesting clockvalues, sening the msg and a unique identifier (id, potential local clock vlaue) NOTE: logic of local clock value might be worng
+        eMessage = new ExampleMessage("req", messagetext, id , localSeqArray[id]+1);
+        send(eMessage);
+    }
 
-        vectorArray[id] ++;
-        messagetext = " -- vector clock value: " + vectorArray[id] + " -- messagetext: " + messagetext;
-
+    /**
+     * Actual method that broadcasts messages
+     * @param message  The message received
+     */
+    private void send(Message message) {
         for(int i=0; i < hosts; i++) {
             /* Sends to everyone except itself */
             if(i != id) {
-                bcom.basicsend(i,new ExampleMessage(id, messagetext, vectorArray[id]));
+                bcom.basicsend(i, message);
             }
         }
-        mcui.debug("Sent out: \""+messagetext+"\"");
-        mcui.deliver(id, messagetext, "from myself!");
+        mcui.debug("Sent out: \""+message.getText()+"\"");
+        mcui.deliver(id, message.getText(), "from myself!");
     }
-    
+
+
+
+
+    /* THIS DIFFERS FROM LAST COURSE IN THE WAY THAT WE HAVE TO CONSIDER HOW TO SEND THE MESSAGES AS WELL AS THE DS ALGO. DID NOT EVEN NOTICE THIS... MAYBE BECAUSE I WE ARE GETTING SO USED TO DS? */
+
+
+
+
     /**
      * Receive a basic message
      * @param message  The message received
@@ -43,32 +75,74 @@ public class ExampleCaster extends Multicaster {
     public void basicreceive(int peer,Message message) {
         ExampleMessage eMessage = (ExampleMessage)message;
 
-        if (eMessage.getVectorValue() != vectorArray[eMessage.getSender()] + 1 ) {
+        //TODO: implement Reliable-broadcast!!!
+
+        // PHASE 1 -- recives request for proposal and message, sending propsal
+        if (eMessage.getStatus().equals("req")) {
+            tmp = Math.max(proposeSeq, globalSeq) + 1;
+            if ((tmp - 1) == globalSeq) { // was global biggest?
+                proposeSeq = tmp;
+                proposeID = tmp;
+            }
+            eMessage = new ExampleMessage("pro", proposeSeq, proposeID);
+        
+        // PHASE 2 -- recives proposal, broadcasting the new message with new correct clock-value
+        // NOTE: does not send the message again, only the unique msg identifier to find the msg in some buffer and what global sequence it should have
+        } else if (eMessage.getStatus().equals("pro"))) {
+            if (proposalArray.get().size() == hosts) {
+                int tmpSeq;
+                int tmpID;
+                for (ExampleMessage eMessage : proposalArray.get()) { 
+                    if (eMessage.getLocal
+                }
+                localSeqArray[id] ++;
+                eMessage = ExampleMessage("seq", id , localSeqArray[id], globalSeq, globalID);
+                send(eMessage);
+            } else {
+                proposalArray.get().add(eMessage)
+            }
+            
+
+        // PHASE 3 -- recives global clovk value/sequence for a message, i.e. actually deliver msg
+        } else if (eMessage.getStatus().equals("seq")) {
+            //code
+            
+
+        // else error
+        } else {
+            mcui.debug("Error - basicrecive() failed to find right phase");
+            
+        }
+
+
+
+
+
+        if (eMessage.getVectorValue() != localSeqArray[eMessage.getSender()] + 1 ) {
 
             // save message to a buffer (one for each sender), this is done if messages are not deleiverd in correct order
             buffer.add(eMessage); 
+            //mcui.debug("value added TO buffer");
         
         } else {
 
             // this if-statement is not really necessary as it would be okey to just overwite the correct value in the array with the same value
             if (eMessage.getSender() != id) { 
-                vectorArray[eMessage.getSender()] = eMessage.getVectorValue();
+                localSeqArray[eMessage.getSender()] = eMessage.getVectorValue();
             } 
             mcui.deliver(peer, eMessage.getText());
 
             // check if buffer messages can be added 
             checkBuffer(eMessage);
 
-            
         }
     }
 
     private void checkBuffer(ExampleMessage latestMsg){
         for (ExampleMessage bufferMessage : buffer) {
-            if (bufferMessage.getSender() == latestMsg.getSender() && bufferMessage.getVectorValue() == vectorArray[latestMsg.getSender()] + 1) {
-
-                vectorArray[bufferMessage.getSender()] = bufferMessage.getVectorValue();
-                System.out.println("Added value from buffer");
+            if (bufferMessage.getSender() == latestMsg.getSender() && bufferMessage.getVectorValue() == localSeqArray[latestMsg.getSender()] + 1) {
+                localSeqArray[bufferMessage.getSender()] = bufferMessage.getVectorValue();
+                mcui.debug("Added value FROM buffer");
                 checkBuffer(bufferMessage);
 
             }
--- lab1/ExampleMessage.java
+++ lab1/ExampleMessage.java
@@ -8,15 +8,41 @@ import mcgui.*;
  */
 public class ExampleMessage extends Message {
         
+    private String status;
     private String text;
-    private int vectorValue;
-        
-    public ExampleMessage(int sender,String text, int vectorValue) {
-        super(sender);
+    private int sequence;
+    private int localSeq;
+    private int localID;
+    private int globalSeq;
+    private int globalID;
+    private int proposeSeq;
+    private int proposeID;
+
+    //messagetext = "req" + ":" + messagetext + ":" + id + ":" + vectorArray[id]+1;
+    public ExampleMessage(String status, String text, int sender, int sequence) {
+        this.status = status;
         this.text = text;
-        this.vectorValue = vectorValue;
+        super(sender);
+        this.sequence = sequence;
     }
-    
+
+    // messagetext = "seq" + ":" + id + ":" + vectorArray[id]+1 ":" + global[0] + ":" + global[1];
+    public ExampleMessage(String status, int localSeq, int localID, int globalSeq, int globalID) {
+        this.status = status;
+        this.localSeq = localSeq;
+        this.localID = localID;
+        this.globalSeq = globalSeq;
+        this.globalID = globalID;
+
+    }
+
+    //messagetext = "pro" + ":" + proposeSeq + ":" + proposeID;
+    public ExampleMessage(String status, int proposeSeq, int proposeID) {
+        this.status = status;
+        this.proposeSeq = proposeSeq;
+        this.proposeID = proposeID;
+    }
+
     /**
      * Returns the text of the message only. The toString method can
      * be implemented to show additional things useful for debugging
@@ -30,8 +56,30 @@ public class ExampleMessage extends Message {
      * Returns vectorVlaue of message, this info together with id of
      * sender complets all info needed for vector clock 
      */
-    public int getVectorValue() {
-        return vectorValue;
+    public int getSequence() {
+        return sequence;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+    public int getLocalSeq(){
+        return localSeq;
+    }
+    public int getLocalID(){
+        return localID;
+    }
+    public int getGlobalSeq(){
+        return globalSeq;
+    }
+    public int getGlobalID(){
+        return globalID;
+    }
+    public int getProposeSeq(){
+        return proposeSeq;
+    }
+    public int getProposeID(){
+        return proposeID;
     }
 
     public static final long serialVersionUID = 0;
